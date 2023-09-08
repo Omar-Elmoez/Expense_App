d70ba4e118b89a5ced71fe8c35b21689896bcf38
Omar Elmoez <omarelmoez2000@gmail.com>
2023-09-08 10:04:46 +0300

    Starting with useState

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,17 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import Card from "../Card/Card";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
 
 
 export default function ExpenseItem({title, amount, date}) {
+  
+  const [price, setPrice] = useState(amount)
+  function clickHandler() {
+    setPrice(prev => prev - 15);
+  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${price}</div>
+        <button onClick={clickHandler}>Apply Discount</button>
       </div>
     </Card>
   );
